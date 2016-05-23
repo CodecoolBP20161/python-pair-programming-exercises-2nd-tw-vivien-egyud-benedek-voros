d823571ac25ef0ab93f522eb13db10fa454aab6c
Vivoca <you@example.com>
2016-05-23 14:15:52 +0200

    years_module.py

--- years/years_module.py
+++ years/years_module.py
@@ -2,10 +2,14 @@ import datetime
 
 
 def years(age):
-    return
+    return datetime.datetime.now().year + 100 - age
 
 
 def main():
+    name = input("Your name: ")
+    age = int(input("Your age: "))
+    number = int(input("Enter a number:"))
+    print(number * (str(years(age)) + "\n"))
     return
 
 
