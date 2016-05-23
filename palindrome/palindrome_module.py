ac47ab1244032cb09bde28255c97dca5afd4cc60
Vivoca <you@example.com>
2016-05-23 15:02:02 +0200

    palindrome_module.py

--- palindrome/palindrome_module.py
+++ palindrome/palindrome_module.py
@@ -1,8 +1,19 @@
 def palindrome(str):
-    return
+    boolean_palindrom = False
+    text_list = list(str)
+    no_space = []
+    for i in range(len(text_list)):
+        if text_list[i] != " ":
+            no_space.append(text_list[i])
+    print(no_space)
+    print (no_space[::-1])
+    # if no_space.reverse() == no_space:
+    #     return boolean_palindrom
 
 
 def main():
+    text = input("Your text:")
+    palindrome(text)
     return
 
 
